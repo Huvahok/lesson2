9bd4ecdab7bf4919a5cc7ddf35057cc08d23bb19
Anton Radenko <fortniate@mail.ru>
2024-03-14 19:19:41 +0300

    add task3:lesson1

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 2, 5, 7, 8, 35, 1, 3, 8, 5, 4};
+int i = 0;
+
+while (i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        System.Console.WriteLine($"{array[i]} ");
+    }
+    i += 1;
+}
